77840892f15fc55bc8d1661b8c7878c65f618c6a
msallese22 <191892761+msallese22@users.noreply.github.com>
2025-03-31 20:31:11 -0400

    started working on code!
    Have a header, a logo in place, and a custom style sheet. routing is also like a little bit in there. yay.

---
+++ Capstone/client/src/app/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {NgOptimizedImage} from '@angular/common';
+
+@Component({
+  selector: 'app-header',
+  imports: [
+    NgOptimizedImage
+  ],
+  templateUrl: './header.component.html',
+  standalone: true,
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+}
