bfd12e725c76a7baee21423a2c11e4c8b22db246
msallese22 <191892761+msallese22@users.noreply.github.com>
2025-04-05 20:53:18 -0400

    got some routing
    and started styling search bar

--- Capstone/client/src/app/home/emp-home/emp-home.component.ts
+++ Capstone/client/src/app/home/emp-home/emp-home.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {MatIconButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-emp-home',
   imports: [
     RouterOutlet,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatIcon,
+    MatIconButton,
   ],
   templateUrl: './emp-home.component.html',
   standalone: true,
