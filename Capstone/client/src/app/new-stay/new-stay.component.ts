e52b5e94f73292a527103ebdf6d515d45cfb8e4f
msallese22 <191892761+msallese22@users.noreply.github.com>
2025-04-07 20:18:17 -0400

    forms!
    this isn't even my final form. the book a new stay form is the back bone of the whole project, pretty much. every other major form is just a derivative of this one.

--- Capstone/client/src/app/new-stay/new-stay.component.ts
+++ Capstone/client/src/app/new-stay/new-stay.component.ts
@@ -4,21 +4,77 @@ import {MatIcon} from '@angular/material/icon';
 import {MatDialog} from '@angular/material/dialog';
 import {SaveStayDetailsModalComponent} from '../shared/save-stay-details-modal/save-stay-details-modal.component';
 import {Router} from '@angular/router';
+import {MatFormField, MatHint, MatInput, MatLabel, MatSuffix} from '@angular/material/input';
+import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from '@angular/material/datepicker';
+import {FormGroup, FormControl, ReactiveFormsModule} from '@angular/forms';
+import {provideNativeDateAdapter} from '@angular/material/core';
 
 @Component({
   selector: 'app-new-stay',
   imports: [
     MatButton,
-    MatIcon
+    MatIcon,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatDatepickerToggle,
+    MatSuffix,
+    MatDatepickerInput,
+    MatDatepicker,
+    ReactiveFormsModule
   ],
   templateUrl: './new-stay.component.html',
+  providers: [provideNativeDateAdapter()],
   standalone: true,
   styleUrl: './new-stay.component.css'
 })
-export class NewStayComponent {
+export class NewStayComponent
+{
   dialog = inject(MatDialog);
-  router= inject(Router);
+  router = inject(Router);
 
+  readonly minDate = new Date();
+  readonly maxDate = new Date(this.minDate.getFullYear() + 1, this.minDate.getMonth(), this.minDate.getDay());
+  readonly checkOutMinDate = new Date(this.minDate.getFullYear(), this.minDate.getMonth(), this.minDate.getDay() + 1);
+
+  king?: boolean | null = null;
+
+
+  newStayForm = new FormGroup({
+    checkInDate: new FormControl(''),
+    checkOutDate: new FormControl(''),
+    guestInfoForm: new FormGroup({
+      guestFirstName: new FormControl(''),
+      guestLastName: new FormControl(''),
+      guestPhone: new FormControl(''),
+      guestEmail: new FormControl(''),
+      guestNotes: new FormControl('')
+    }),
+    roomType: new FormControl(''),
+    creditCardInfoForm: new FormGroup({
+      creditCardNumber: new FormControl(''),
+      creditCardExp: new FormControl(''),
+      creditCardCVV: new FormControl('')
+    })
+  });
+
+  get guestInfoFormGroup()
+  {
+    return this.newStayForm.get('guestInfoForm') as FormGroup;
+  }
+
+  get creditCardInfoFormGroup()
+  {
+    return this.newStayForm.get('creditCardInfoForm') as FormGroup;
+  }
+
+  setRoomType(roomType: string)
+  {
+    this.newStayForm.get("roomType")?.setValue(roomType);
+
+    this.king=!this.king;
+
+  }
 
   openDialog()
   {
@@ -27,8 +83,9 @@ export class NewStayComponent {
       data: {
         isCreate: true
       }
-    })
-    dialogRef.afterClosed().subscribe(() => {
+    });
+    dialogRef.afterClosed().subscribe(() =>
+    {
       this.router.navigateByUrl("/home");
     });
   }
